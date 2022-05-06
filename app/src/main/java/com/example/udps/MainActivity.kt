28080bdf539bd38277dae6393f41ffde242bda28
chris <jmuir5@myune.edu.au>
2022-05-06 14:28:29 +1000

    first attempt at implementing realm shit

--- app/src/main/java/com/example/udps/MainActivity.kt
+++ app/src/main/java/com/example/udps/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.udps
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+
+import io.realm.log.RealmLog
+import io.realm.mongodb.Credentials
 
 /**
  * log in screen code
@@ -14,16 +19,75 @@ import android.widget.Toast
  * and twin_mum, a test account to work out a parent with multiple children.
  */
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var username: EditText
+    private lateinit var password: EditText
+    private lateinit var loginButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        username = findViewById(R.id.editTextUser)
+        password = findViewById(R.id.editTextPass)
+        loginButton = findViewById<Button>(R.id.loginBtn)
         //attach an action to the log in screen button
-        val loginBtn = findViewById<Button>(R.id.loginBtn)
-        loginBtn.setOnClickListener{
-            validate(findViewById<EditText>(R.id.editTextUser).text.toString(), findViewById<EditText>(R.id.editTextPass).text.toString())
+
+        loginButton.setOnClickListener{
+            login()
         }
     }
 
+    override fun onBackPressed() {
+        // Disable going back to the MainActivity
+        moveTaskToBack(true)
+    }
+
+    private fun onLoginSuccess(partition: String) {
+        // successful login ends this activity, bringing the user back to the task activity
+        val sharedPreference =  getSharedPreferences("prefs name", Context.MODE_PRIVATE)
+        var editor = sharedPreference.edit()
+        editor.putString("partition",partition)
+        editor.commit()
+        finish()
+    }
+    private fun onLoginFailed(errorMsg: String) {
+        Log.v(TAG(), errorMsg)
+        Toast.makeText(baseContext, errorMsg, Toast.LENGTH_LONG).show()
+    }
+    private fun validateCredentials(): Boolean = when {
+        // zero-length usernames and passwords are not valid (or secure), so prevent users from creating accounts with those client-side.
+        username.text.toString().isEmpty() -> false
+        password.text.toString().isEmpty() -> false
+        else -> true
+    }
+
+    private fun login() {
+        if (!validateCredentials()) {
+            onLoginFailed("Invalid username or password")
+            return
+        }
+
+        // while this operation completes, disable the buttons to login or create a new account
+        loginButton.isEnabled = false
+
+        val username = this.username.text.toString()
+        val password = this.password.text.toString()
+
+
+
+        val creds = Credentials.emailPassword(username, password)
+        UDPSApp.loginAsync(creds) {
+            // re-enable the buttons after
+            loginButton.isEnabled = true
+            if (!it.isSuccess) {
+                RealmLog.error(it.error.toString())
+                onLoginFailed(it.error.message ?: "An error occurred.")
+            } else {
+                onLoginSuccess("test")
+            }
+        }
+
+    }
     //log in validation function. will have to be changed completely at some point
     fun validate(username:String, password:String){
         var accounts = arrayOf(arrayOf("kerry", "abc123", "teacher", "teacher"),
